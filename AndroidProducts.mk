65cb35a124e3451a56ef67d92c29953acec328eb
Vincent4440 <aditya.singh2008v@gmail.com>
2023-07-08 11:57:01 +0530

    munch: Project-Zephyrus Bringup

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 Project Zephyrus
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/munch.mk
+
+COMMON_LUNCH_CHOICES := \
+    munch-user \
+    munch-userdebug \
+    munch-eng
---
+++ munch.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 Project Zephyrus
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile
+$(call inherit-product, device/xiaomi/munch/device.mk)
+
+# Inherit from the zephyrus configuration.
+$(call inherit-product, vendor/zephyrus/target/product/zephyrus-target.mk)
+
+# Set Boot Animination Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := munch
+PRODUCT_DEVICE := munch
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F4
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := Redmi/munch/munch:13/RKQ1.211001.001/V14.0.3.0.TLMMIXM:user/release-keys
